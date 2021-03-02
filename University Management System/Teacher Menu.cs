df7ed9e8fa96f4f316b5a05c052c94dd47b4c679
Nazmur Raihan <nazmurraihan46@gmail.com>
2021-03-02 22:23:12 +0600

    Teacher Result done

--- University Management System/Teacher Menu.cs
+++ University Management System/Teacher Menu.cs
@@ -41,8 +41,8 @@ namespace University_Management_System
         }
         public void Auto_MarksInsertcoursecode()
         {
-            con.dataGet("Select ccode from Teacher_Course"); //  '" +  +"'
-                                                             //}                                                                               //con.dataGet("Select * from Admin where id='" + aname.Text + "' and pass='" + apass.Text + "'"); //  '" +  +"'
+            con.dataGet("Select ccode from Teacher_Course where id='" + tchcode + "'"); //  '" +  +"'
+                                                                                        //}                                                                               //con.dataGet("Select * from Admin where id='" + aname.Text + "' and pass='" + apass.Text + "'"); //  '" +  +"'
             DataTable dtsuggeststulistccode = new DataTable();
             con.sda.Fill(dtsuggeststulistccode);
 
@@ -86,19 +86,19 @@ namespace University_Management_System
             insertMarksstuid.AutoCompleteSource = AutoCompleteSource.CustomSource;
             insertMarksstuid.AutoCompleteCustomSource = coll;
         }
-        
+
         private void tchClist_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            
+
         }
         //Shows Course List
         private void CourseList_LoadData()
         {
-            
+
             con.dataGet("Select * from Course,Teacher_Course where Teacher_Course.id= '" + tchcode + "' and Teacher_Course.ccode=Course.ccode");
             DataTable courselist = new DataTable();
             con.sda.Fill(courselist);
-            foreach(DataRow row in courselist.Rows)
+            foreach (DataRow row in courselist.Rows)
             {
                 int n = tchClist.Rows.Add();
                 tchClist.Rows[n].Cells["tchCserialnogrid"].Value = n + 1;
@@ -125,7 +125,7 @@ namespace University_Management_System
             con.dataGet("Select * from Teacher where id='" + tchcode + "' ");
             DataTable tchinfo = new DataTable();
             con.sda.Fill(tchinfo);
-            string Tchname,Tcode, Tpos,Tdept;
+            string Tchname, Tcode, Tpos, Tdept;
             Tchname = "Name : ";
             Tcode = "Teacher Code : ";
             Tpos = "Position : ";
@@ -149,7 +149,7 @@ namespace University_Management_System
 
         private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if(tabControl1.SelectedIndex==0 && tabControl3.SelectedIndex==0)
+            if (tabControl1.SelectedIndex == 0 && tabControl3.SelectedIndex == 0)
             {
                 if (tchClist.Rows.Count == 0)
                 {
@@ -160,9 +160,9 @@ namespace University_Management_System
 
         private void tabControl3_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if(tabControl3.SelectedIndex==0)
+            if (tabControl3.SelectedIndex == 0)
             {
-                if (tchClist.Rows.Count  == 0)
+                if (tchClist.Rows.Count == 0)
                 {
                     MessageBox.Show("You haven't been Assigned to any course yet", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
@@ -171,13 +171,13 @@ namespace University_Management_System
 
         private void tchstulistSrchbtn_Click(object sender, EventArgs e)
         {
-            con.dataGet("Select Student.id,Student.sname,Student.sintake,Student.ssection,Student.sdept,Student.sprog,Student.smobile from Teacher_Course,Student,Student_Course where Student.id=Student_Course.id and Student_Course.tid=Teacher_Course.id and Teacher_Course.ccode='"+tchCcode.Text+"' and Teacher_Course.tsemester='"+tchCsemester.Text+ "' and Student_Course.ccode='" + tchCcode.Text + "' and Student_Course.ssemester='" + tchCsemester.Text + "' and Student_Course.tid = '"+tchcode+"'");
+            con.dataGet("Select Student.id,Student.sname,Student.sintake,Student.ssection,Student.sdept,Student.sprog,Student.smobile from Teacher_Course,Student,Student_Course where Student.id=Student_Course.id and Student_Course.tid=Teacher_Course.id and Teacher_Course.ccode='" + tchCcode.Text + "' and Teacher_Course.tsemester='" + tchCsemester.Text + "' and Student_Course.ccode='" + tchCcode.Text + "' and Student_Course.ssemester='" + tchCsemester.Text + "' and Student_Course.tid = '" + tchcode + "'");
             DataTable stulist = new DataTable();
             con.sda.Fill(stulist);
             tchstuGrid.Rows.Clear();
-            if(stulist.Rows.Count>0)
+            if (stulist.Rows.Count > 0)
             {
-                foreach(DataRow row in stulist.Rows)
+                foreach (DataRow row in stulist.Rows)
                 {
                     int n = tchstuGrid.Rows.Add();
                     tchstuGrid.Rows[n].Cells["stulistserialnogrid"].Value = n + 1;
@@ -206,13 +206,14 @@ namespace University_Management_System
         {
             marksemester = insertMarktsemester.Text;
             mcode = insertMarksccode.Text;
-            con.dataGet("Select Student.id,Student.sname,Student.sintake,Student.ssection,Student_Course.ccode from Student,Student_Course,Teacher_Course Where Student_Course.ssemester = Teacher_Course.tsemester and Student_Course.ccode=Teacher_Course.ccode and  Student.id=Student_Course.id and Student_Course.tid=Teacher_Course.id and Student_Course.tid = '" + tchcode+"' and Student_Course.ccode = '"+ insertMarksccode.Text +"' and Student_Course.ssemester = '"+insertMarktsemester.Text+"'");
+            con.dataGet("Select Student.id,Student.sname,Student.sintake,Student.ssection,Student_Course.ccode from Student,Student_Course,Teacher_Course Where Student_Course.ssemester = Teacher_Course.tsemester and Student_Course.ccode=Teacher_Course.ccode and  Student.id=Student_Course.id and Student_Course.tid=Teacher_Course.id and Student_Course.tid = '" + tchcode + "' and Student_Course.ccode = '" + insertMarksccode.Text + "' and Student_Course.ssemester = '" + insertMarktsemester.Text + "'");
             DataTable Stulist = new DataTable();
             con.sda.Fill(Stulist);
+            marksListgrid.Rows.Clear();
             marksStulistgrid.Rows.Clear();
-            if(Stulist.Rows.Count > 0)
+            if (Stulist.Rows.Count > 0)
             {
-                foreach(DataRow row in Stulist.Rows)
+                foreach (DataRow row in Stulist.Rows)
                 {
                     int n = marksStulistgrid.Rows.Add();
                     marksStulistgrid.Rows[n].Cells["insertMslgrid"].Value = n + 1;
@@ -260,6 +261,7 @@ namespace University_Management_System
                 MessageBox.Show("Marks Successfully Inserted.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 marksListgrid_LoadData();
             }
+
         }
 
         private void marksListgrid_LoadData()
@@ -270,10 +272,10 @@ namespace University_Management_System
             marksListgrid.Rows.Clear();
             if (Markt.Rows.Count > 0)
             {
-                foreach(DataRow row in Markt.Rows)
+                foreach (DataRow row in Markt.Rows)
                 {
                     int n = marksListgrid.Rows.Add();
-                    marksListgrid.Rows[n].Cells["markslgrid"].Value = n+1;
+                    marksListgrid.Rows[n].Cells["markslgrid"].Value = n + 1;
                     marksListgrid.Rows[n].Cells["marksidgrid"].Value = row["id"].ToString();
                     marksListgrid.Rows[n].Cells["markmidgrid"].Value = row["mid"].ToString();
                     marksListgrid.Rows[n].Cells["markfinalgrid"].Value = row["final"].ToString();
@@ -283,17 +285,17 @@ namespace University_Management_System
                     string final = row["final"].ToString();
                     string of25 = row["outof25"].ToString();
                     string att = row["attendence"].ToString();
-                    int M, F, OF25, A,AN, Total;
+                    int M, F, OF25, A, AN, Total;
                     M = int.Parse(mid);
                     F = int.Parse(final);
                     OF25 = int.Parse(of25);
                     A = int.Parse(att);
                     if (A >= 80) AN = 5;
-                    else if(A<=79 && A>=70) AN = 4;
-                    else if(A<=69 && A>=60) AN = 3;
-                    else if(A<=59 && A>=55) AN = 2;
+                    else if (A <= 79 && A >= 70) AN = 4;
+                    else if (A <= 69 && A >= 60) AN = 3;
+                    else if (A <= 59 && A >= 55) AN = 2;
                     else AN = 1;
-                    Total = (M+(F+(OF25 + AN)));
+                    Total = (M + (F + (OF25 + AN)));
                     string grade;
                     //80+ = A+ =4.00
                     //75-79 = A =3.75
@@ -322,7 +324,7 @@ namespace University_Management_System
             }
             else
             {
-                MessageBox.Show("There is no Marks inserted yet.","Information",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                MessageBox.Show("There is no Marks inserted yet.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
 
@@ -343,15 +345,15 @@ namespace University_Management_System
         }
         private void tch_Setconfirmbtn_Click(object sender, EventArgs e)
         {
-            if (set_Newpass.Text != "" && set_Conpass.Text != "") 
+            if (set_Newpass.Text != "" && set_Conpass.Text != "")
             {
                 con.dataGet("select * from Teacher Where id = '" + tchcode + "' and pass = '" + set_Oldpass.Text + "'");
                 DataTable oc = new DataTable();
                 con.sda.Fill(oc);
 
-                if(oc.Rows.Count > 0)
+                if (oc.Rows.Count > 0)
                 {
-                    if(set_Newpass.Text == set_Conpass.Text)
+                    if (set_Newpass.Text == set_Conpass.Text)
                     {
                         if (set_Newpass.Text != "" + tchcode + "bubt" && set_Conpass.Text != "" + tchcode + "bubt")
                         {
@@ -386,12 +388,12 @@ namespace University_Management_System
                 }
                 else
                 {
-                    MessageBox.Show("Old password doesn't match!","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    MessageBox.Show("Old password doesn't match!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
             else
             {
-                MessageBox.Show("New Password and Confirm Password can't be blank!","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                MessageBox.Show("New Password and Confirm Password can't be blank!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
@@ -441,6 +443,147 @@ namespace University_Management_System
                 //set_Newpass.UseSystemPasswordChar = true;
                 set_Conpass.UseSystemPasswordChar = true;
             }
+        }
+
+        private void res_Srchbtn_Click(object sender, EventArgs e)
+        {
+            con.dataGet("Select Student.id,Student.sname,Student.sintake,Student.ssection,Student.sdept,mid,final,outof25,attendence from Result,Student_Course,Student,Teacher_Course where Student_Course.ccode=Teacher_Course.ccode and Student_Course.ssemester=Teacher_Course.tsemester and Student_Course.id=Student.id and Result.tid=Teacher_Course.id and Result.id=Student_Course.id and Result.ccode=Teacher_Course.ccode and Result.ccode=Student_Course.ccode and Result.rsemester=Teacher_Course.tsemester and Result.rsemester=Student_Course.ssemester and Student_Course.tid='" + tchcode + "' and Teacher_Course.ccode='" + res_Ccode.Text + "' and Teacher_Course.tsemester='" + res_semester.Text + "' and Result.tid='" + tchcode + "' and Result.ccode='" + res_Ccode.Text + "' and Result.rsemester='" + res_semester.Text + "'");
+            //con.dataGet("Select * from Result where ccode = '" + res_Ccode.Text + "' and rsemester = '" + res_semester + "' and tid = '" + tchcode + "'");
+            DataTable tchres = new DataTable();
+            con.sda.Fill(tchres);
+            res_Grid.Rows.Clear();
+            if (tchres.Rows.Count > 0)
+            {
+                foreach (DataRow row in tchres.Rows)
+                {
+                    int n = res_Grid.Rows.Add();
+                    res_Grid.Rows[n].Cells["tchResslgrid"].Value = n + 1;
+                    res_Grid.Rows[n].Cells["tchResstuidgrid"].Value = row["id"].ToString();
+                    res_Grid.Rows[n].Cells["tchResstunamegrid"].Value = row["sname"].ToString();
+                    res_Grid.Rows[n].Cells["tchResintakegrid"].Value = row["sintake"].ToString();
+                    res_Grid.Rows[n].Cells["tchRessectiongrid"].Value = row["ssection"].ToString();
+                    res_Grid.Rows[n].Cells["tchResdeptgrid"].Value = row["sdept"].ToString();
+                    res_Grid.Rows[n].Cells["tchResmidgrid"].Value = row["mid"].ToString();
+                    res_Grid.Rows[n].Cells["tchResfinalgrid"].Value = row["final"].ToString();
+                    res_Grid.Rows[n].Cells["tchResoutof25grid"].Value = row["outof25"].ToString();
+                    res_Grid.Rows[n].Cells["tchResattendencegrid"].Value = row["attendence"].ToString();
+                    string mid = row["mid"].ToString();
+                    string final = row["final"].ToString();
+                    string of25 = row["outof25"].ToString();
+                    string att = row["attendence"].ToString();
+                    float M, F, OF25, A, AN, Total;
+                    M = float.Parse(mid);
+                    F = float.Parse(final);
+                    OF25 = float.Parse(of25);
+                    A = int.Parse(att);
+                    if (A >= 80) AN = 5;
+                    else if (A <= 79 && A >= 70) AN = 4;
+                    else if (A <= 69 && A >= 60) AN = 3;
+                    else if (A <= 59 && A >= 55) AN = 2;
+                    else AN = 1;
+                    Total = (M + (F + (OF25 + AN)));
+                    string grade;
+                    //80+ = A+ =4.00
+                    //75-79 = A =3.75
+                    //70 - 74 =A- = 3.50
+                    //65-69 = B+ = 3.25
+                    //60-64 = B = 3.00
+                    //55 - 59 =B- = 2.75
+                    //50-54 = C+ = 2.50
+                    //45-59 = C = 2.25
+                    //40-44 = D = 2.00
+                    //<40 = F
+                    if (Total >= 80) grade = "A+";
+                    else if (Total <= 79 && Total >= 75) grade = "A";
+                    else if (Total <= 74 && Total >= 70) grade = "A-";
+                    else if (Total <= 69 && Total >= 65) grade = "B+";
+                    else if (Total <= 64 && Total >= 60) grade = "B";
+                    else if (Total <= 59 && Total >= 55) grade = "B-";
+                    else if (Total <= 54 && Total >= 50) grade = "C+";
+                    else if (Total <= 49 && Total >= 45) grade = "C";
+                    else if (Total <= 44 && Total >= 40) grade = "D";
+                    else grade = "F";
+
+                    res_Grid.Rows[n].Cells["tchRestotalgrid"].Value = Total.ToString();
+                    res_Grid.Rows[n].Cells["tchResgradegrid"].Value = grade;
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Result hasn't been published yet for this course.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void insertMarksstumid_TextChanged(object sender, EventArgs e)
+        {
+            if (insertMarksstumid.Text != "")
+            {
+                float mid = 0;
+                mid = float.Parse(insertMarksstumid.Text);
+                if (mid > 30)
+                {
+                    MessageBox.Show("Mid marks can't exceed 30!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void insertMarksstufinal_TextChanged(object sender, EventArgs e)
+        {
+            if (insertMarksstufinal.Text != "")
+            {
+                float final = 0;
+                final = float.Parse(insertMarksstufinal.Text);
+                if (final > 40)
+                {
+                    MessageBox.Show("Final marks can't exceed 40!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void insertMarksstuct_TextChanged(object sender, EventArgs e)
+        {
+            if (insertMarksstuct.Text != "")
+            {
+                float outof25 = 0;
+                outof25 = float.Parse(insertMarksstuct.Text);
+                if (outof25 > 25)
+                {
+                    MessageBox.Show("Out of 25 marks can't exceed 25!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void insertMarksstuatten_TextChanged(object sender, EventArgs e)
+        {
+            if (insertMarksstuatten.Text != "")
+            {
+                int attendence = 0;
+                attendence = int.Parse(insertMarksstuatten.Text);
+
+
+                if (attendence > 100)
+                {
+                    MessageBox.Show("Attendence percentage can't exceed 100!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+
+        private void insertMarksstuatten_Enter(object sender, EventArgs e)
+        {
+            if (insertMarksstuatten.Text == "(%)")
+            {
+                insertMarksstuatten.Text = "";
+
+                insertMarksstuatten.TextAlign = HorizontalAlignment.Left;
+                insertMarksstuatten.ForeColor = Color.Black;
+            }
+        }
+
+        private void insertMarksstuatten_Leave(object sender, EventArgs e)
+        {
+            
         }
     }
 }
